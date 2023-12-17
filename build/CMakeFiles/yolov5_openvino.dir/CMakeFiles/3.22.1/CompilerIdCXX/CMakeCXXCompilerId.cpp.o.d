0b1679ca3debd116a560e7e88ffe9a18d380b63a
Hao Feng <2410772045@qq.com>
2023-12-17 20:18:44 +0800

    Delete build directory

--- build/CMakeFiles/yolov5_openvino.dir/CMakeFiles/3.22.1/CompilerIdCXX/CMakeCXXCompilerId.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/yolov5_openvino.dir/CMakeFiles/3.22.1/CompilerIdCXX/CMakeCXXCompilerId.cpp.o: \
- /home/wxxz/workspace/Yolov5-With-OpenVINO/build/CMakeFiles/3.22.1/CompilerIdCXX/CMakeCXXCompilerId.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/yolov5_openvino.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for yolov5_openvino.
-# This may be replaced when dependencies are built.
